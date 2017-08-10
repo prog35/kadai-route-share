2b6ac7fc4f894b57f921184738e7342bfe3badc6
taka8 <learning.prog35@gmail.com>
2017-08-10 14:30:07 +0000

    map edit

--- resources/views/routes/create.blade.php
+++ resources/views/routes/create.blade.php
@@ -1,3 +1,4 @@
+<!--http://matsup.blogspot.jp/2012/05/google-map-2.html-->
 <!DOCTYPE html> 
 <html lang="ja">
 <head>
@@ -6,189 +7,192 @@
     <link rel="stylesheet" type="text/css" href="../css/style2.css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="http://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAPS_JS_ID') }}&libraries=places" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(function(){
-            var renderFLG=false;
-            var directionsDisplay;
-            var directionsService=new google.maps.DirectionsService();
-            var map,mode;
-            var currentDirections=null;
-            var startSpot="東京駅";
-            var endSpot="六本木ヒルズ";
-
-            initialize();
-
-            /* 地図初期化 */
-            function initialize() {
-                var myOptions={
-                    zoom:14,
-                    center: new google.maps.LatLng(35.670236,139.749832),//虎の門
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-                /* 地図オブジェクト生成 */
-                map=new google.maps.Map(document.getElementById("map"), myOptions);
-                if(!renderFLG) render();
-                // calcRoute(startSpot,endSpot);
-            }
-            /* ルート検索結果を描画 */
-            function render(){
-                dbg("render:"+renderFLG);
-                renderFLG=true;
-                /* ルートをレンダリング */
-                directionsDisplay=new google.maps.DirectionsRenderer({
-                    "map": map,
-                    "preserveViewport": true,
-                    "draggable": true
-                });
-                /* 右カラムにルート表示 */
-                directionsDisplay.setPanel(document.getElementById("directions_panel"));
-                /* 出発地点・到着地点マーカーが移動された時 */
-                google.maps.event.addListener(directionsDisplay, 'directions_changed',function() {
-                    currentDirections=directionsDisplay.getDirections();
-                    var route=currentDirections.routes[0];
-                    var s="";
-                    for(var i=0; i<route.legs.length; i++) {
-                        var routeSegment=i+1;
-                        s+=route.legs[i].start_address+'to';
-                        s+=route.legs[i].end_address+'\n';
-                        s+=route.legs[i].distance.text;
-                    }
-                    dbg("directions_changed:"+s);
-                });
-            }
-            /* モード変更 */
-            $("#mode").bind("change",function(){
-                $(".button-group button").removeClass("active");
-                calcRoute(startSpot,endSpot);
-                $("#show").addClass("active");
-            });
-            /* ルート算出 */
-            function calcRoute(startSpot,endSpot){
-                switch($("#mode").val()){
-                    case "driving":
-                        mode=google.maps.DirectionsTravelMode.DRIVING;
-                        break;
-                    case "bicycling":
-                        mode=google.maps.DirectionsTravelMode.BICYCLING;
-                        break;
-                    case "transit":
-                        mode=google.maps.DirectionsTravelMode.TRANSIT;
-                        break;
-                    case "walking":
-                        mode=google.maps.DirectionsTravelMode.WALKING;
-                        break;
-                }
-                if(!renderFLG) render();
-                var request={
-                    origin:startSpot,            /* 出発地点 */
-                    destination:endSpot,        /* 到着地点 */
-                    travelMode:mode                /* 交通手段 */
-                };
-                /* ルート描画 */
-                directionsService.route(request, function(response, status) {
-                    if (status==google.maps.DirectionsStatus.OK) {
-                        dbg(response);
-                        directionsDisplay.setDirections(response);
-                    }else{
-                        dbg("status:"+status);
-                    }
-                });
-            }
-            /* ルート表示・非表示切り替え */
-            $(".button-group button").click(function(e){
-                $(".button-group button").removeClass("active");
-                var id=$(this).attr("id");
-                if(id=="show"){
-                    calcRoute(startSpot,endSpot);
-                    $(this).addClass("active");
-                }else{
-                    $(this).addClass("active");
-                    reset();
-                }
-            });
-            /* ルート削除 */
-            function reset(){
-                currentDirections=null;
-                directionsDisplay.setMap(null);
-                renderFLG=false;
-            }
-	        
-	        $("#mapButton").click(function(e){
-	        	alert(startSpot);
-	        	startSpot = document.getElementById('mapFrom').value;
-	    		endSpot = document.getElementById('mapTo').value;
-	    		calcRoute(startSpot,endSpot);
+	<script type="text/javascript">
+	    var map;
+	    var centerLatLng = new google.maps.LatLng(35.0, 136.5);
+	    var rendererOptions = { draggable: true };
+	    var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+	    var directionsService = new google.maps.DirectionsService();
+	
+		// Direction service 結果のステータス
+		// OK, MAX_WAYPOINTS_EXCEEDED, NOT_FOUND, INVALID_REQUEST, 
+		// OVER_QUERY_LIMIT, REQUEST_DENIED, UNKNOWN_ERROR, ZERO_RESULTS
+	    var direcStat = google.maps.DirectionsStatus; 
+	    var direcErr = new Array(); //ルート結果のエラーメッセージ
+	        direcErr[direcStat.INVALID_REQUEST] = "DirectionsRequest が無効";
+	        direcErr[direcStat.MAX_WAYPOINTS_EXCEEDED] = "経由点がが多すぎます。経由点は 8 以内です。";
+	        direcErr[direcStat.NOT_FOUND] = "いずれかの点が緯度経度に変換できませんでした。";
+	        direcErr[direcStat.OVER_QUERY_LIMIT] = "単位時間当りのリクエスト制限回数を超えました。";
+	        direcErr[direcStat.REQUEST_DENIED] = "このサイトからはルートサービスを使用できません。";
+	        direcErr[direcStat.UNKNOWN_ERROR] = "不明なエラーです。もう一度試すと正常に処理される可能性があります。";
+	        direcErr[direcStat.ZERO_RESULTS] = "ルートを見つけられませんでした。";  
+	
+	    function initialize() {
+	        var myOptions = {
+	            zoom: 9,
+	            center: centerLatLng,
+	            mapTypeId: google.maps.MapTypeId.ROADMAP,
+	            scaleControl: true,
+	            scaleControlOptions: { position: google.maps.ControlPosition.BOTTOM_CENTER }
+	        }
+	        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	        directionsDisplay.setMap(map);
+	        directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+	    
+	        google.maps.event.addListener(map, 'click', function(mouseEvent) {
+	            setPoints(map, mouseEvent.latLng);
 	        });
-	  
-        });
-        var dbg=function(str){
-            try{
-                if(window.console && console.log){
-                    console.log(str);
-                }
-            }catch(err){
-                //alert("error:"+err);
-            }
-        }
-
-            
-        function getPlace(){
-		    var mapFrom = document.getElementById('mapFrom');
-		    if(mapFrom.value){
-		        var service = new google.maps.places.PlacesService(map);
-		        var searchValue = mapFrom.value;
-		        var placeRequest = {
-		            query: searchValue, //入力したテキスト
-		        }
-		 
-		        //リクエストを送ってあげるとプライス情報を格納したオブジェクトを返してくれます。
-		        service.textSearch(placeRequest,function(results,status){
-		            var places = results[0];
-		            toGeocode(places);
-		        });
-		    }
-		}
-		function toGeocode(places){
-		    //取得したplacesオブジェクトから緯度と経度をgeocodeとして渡します。
-		    var latlng = new google.maps.LatLng(places.geometry.location.lat(),places.geometry.loca.lng());
-		    //ルート取得
-		    getRoute(latlng);
-		}
-    </script>
-    <style>
-        #map { float:left; width:70%; height:100%; }
-        #side { float:right; width:30%; height:100%; }
-        #side .inner { padding:10px; overflow:auto; }
-    </style>
-</head>
-<body>
-
-    <div class="searchBox">
-	   
-	       <input id="mapFrom" type="text">
-	       <input id="mapTo" type="text">
-	       <button id="mapButton">検索</button>
-	   
-	</div>
-    
-    <div id="map"><!-- 地図の埋め込み表示 --></div>
-    <div id="side">
-        <div class="inner">
-        <p>
-            <label for="mode">モード：<select id="mode" name="mode">
-                <option value="driving" selected>DRIVING（自動車）</option>
-                <option value="bicycling">BICYCLING （自転車）</option>
-                <option value="transit">TRANSIT（電車）</option>
-                <option value="walking">WALKING（徒歩）</option>
-            </select></label>
-        </p>
-        <div class="button-group">
-            <button id="show" class="button active">ルート表示</button>
-            <button id="hide" class="button">ルート非表示</button>
-        </div>
-        <div id="directions_panel" style="width:100%"></div>
-    </div>
-    </div><!-- #side -->
-    <br clear="all" />
-</body>
+	        google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+	            modifyDirectionDataProcess(directionsDisplay.directions);
+	        });
+	    }
+		// ==================================================
+	    function displayRouteString(result) {
+	        var strData = '';
+	        if (document.getElementById("orderLngLat").checked) {
+	            document.getElementById("orderCoord").textContent = "Lng-Lat";
+	            document.getElementById("orderCoord").style.color = "red";
+	        } else {
+	            document.getElementById("orderCoord").textContent = "Lat-Lng";
+	            document.getElementById("orderCoord").style.color = "blue";
+	        }
+	        for (var j = 0; j < (result.routes.length); j++) {
+	            for (var i = 0; i < (result.routes[j].overview_path.length); i++) {
+	                var lat = result.routes[j].overview_path[i].lat().toFixed(9);
+	                var lng = result.routes[j].overview_path[i].lng().toFixed(9);
+	                if (document.getElementById("orderLngLat").checked) { strData += lng+','+lat+"\n"; }
+	                                                               else { strData += lat+','+lng+"\n"; }
+	            } // for i
+	        } // for j
+	        
+	        
+	        document.getElementById("info_window").value = strData;
+	    }
+	// --------------------------------------------------
+	    function modifyDirectionDataProcess(result) {
+	        var total = 0;
+	        var myroute = result.routes[0];
+	        for (i = 0; i < myroute.legs.length; i++) { total += myroute.legs[i].distance.value; }
+	        total = total / 1000;
+	        document.getElementById("total").innerHTML = total + " km";
+	        displayRouteString(result);
+	    }
+	// --------------------------------------------------
+	    function setPoints(map, latlng) {
+	        var geocoder = new google.maps.Geocoder();
+	        var strData;
+	        if (document.getElementById("start").checked) { 
+	            document.getElementById("start_pts").value = latlng.lat().toFixed(9)+","+latlng.lng().toFixed(9);
+	        }
+	        if (document.getElementById("end").checked) { 
+	            document.getElementById("end_pts").value = latlng.lat().toFixed(9)+","+latlng.lng().toFixed(9);
+	        }
+	        if (document.getElementById("waypoint").checked) {
+	            strData = document.getElementById("waypoints").value;
+	            strData += latlng.lat().toFixed(9)+","+latlng.lng().toFixed(9)+"\n";
+	            document.getElementById("waypoints").value = strData;
+	        }
+	    }
+	// --------------------------------------------------
+	    function clearAddr() {
+	        document.getElementById("start_pts").value = '';
+	        document.getElementById("end_pts").value = '';
+	        document.getElementById("waypoints").value = '';
+	    }
+	// --------------------------------------------------
+	    function calcRoute() {
+	        var start = document.getElementById("start_pts").value;
+	        var end = document.getElementById("end_pts").value;
+	        var hw_flag;
+	        var toll_flag;
+	        var strData = '';
+	
+	        var waypts = [];
+	        var waypoints = document.getElementById("waypoints").value;
+	        var wptsArray = waypoints.split("\n");
+	        for (var i = 0; i < wptsArray.length; i++) {
+	            if (wptsArray[i] != '') { waypts.push({location:wptsArray[i], stopover:true}); }
+	        }
+	
+	        if (document.getElementById("nonhighway").checked) { hw_flag = true; } else { hw_flag = false; }
+	        if (document.getElementById("nontollway").checked) { toll_flag = true; } else { toll_flag = false; }
+	
+	        var request = {
+	            origin: start, 
+	            destination: end,
+	            waypoints: waypts,
+	            optimizeWaypoints: true,
+	            avoidHighways: hw_flag,
+	            avoidTolls: toll_flag,
+	            travelMode: google.maps.DirectionsTravelMode.DRIVING
+	        };
+	        directionsService.route(request, function(response, status) {
+	            if (status == google.maps.DirectionsStatus.OK) {
+	                directionsDisplay.setDirections(response);
+	                displayRouteString(response);
+	            } else { alert("Directions Service ERROR : "+status+"\n"+direcErr[status]); }
+	        });
+	    }
+	// --------------------------------------------------
+	</script>
+	</head>
+	
+	<body bgcolor="#D0D0D0" onload="initialize()">
+	  <div id="map_canvas" style="float:left;width:74%;height:100%;"></div>
+	  <div id="control_panel" 
+	          style="float:right;width:26%;text-align:left;padding-top:20px;font-size:small;">
+	    <font class="boldblack" size="+1">　2点間の経路検索</font>　radio ボタン＋クリックで指定
+	    <table border="0" cellpadding="2" cellspacing="0">
+	      <tr><td colspan="4" style="font-size:small;">
+	      　・各項目は<font class="blue">住所・名称の直接入力可</font>。
+	        <font class="black">Lat,Lng (緯度,経度) の方が精度高</font><br>
+	      　・検索後，<font class="blue">ドラッグによる経路変更可</font>。
+	        <font class="black">経路点数値データも自動修正</font><br>
+	      </td></tr>
+	    </table>
+	    <hr size="1" class="lightgray">
+	    <form name="select_points">
+	      <table border="0" cellpadding="1" cellspacing="0">
+	        <tr><td style="font-size:small;"> 
+	          <input type="radio" name="select_points" 
+	                    style="font-size:small;" id="start" checked>出発点</td>
+	          <td><input type="text" size="38" id="start_pts"></td></tr>
+	        <tr><td style="font-size:small;"> 
+	          <input type="radio" name="select_points" style="font-size:small;" id="end">到着点</td>
+	          <td><input type="text" size="38" id="end_pts"></td></tr>
+	        <tr><td style="font-size:small;"> 
+	          <input type="radio" name="select_points" id="waypoint">経由地</td>
+	          <td><textarea cols="36" rows="7" id="waypoints" style="font-size:small;"></textarea></td></tr>
+	
+	        <tr><td colspan="4" align="center" style="font-size:small;">
+	     　   <input type="checkbox" id="nonhighway" class="black">高速道路除く　　
+	        　<input type="checkbox" id="nontollway" class="black">有料道路除く
+	        </td></tr>
+	        <tr><td colspan="4" align="center">
+	        　<input type="button" class="boldred100" onclick="calcRoute();" value="経路を求める">　　
+	        　<input type="button" class="boldblue100" onclick="clearAddr();" value="入力値クリア">
+	        </td></tr>
+	      </table>
+	    </form>
+	
+        <hr size="1" class="lightgray">
+	    <table border="0" cellpadding="4" cellspacing="0">
+	      <tr>
+      		<td style="font-size:small;">　Info_Window　経路検索後は経路の <span id="orderCoord" style="color:blue">Lat-Lng</span> 順　
+			 （<input type="checkbox" id="orderLngLat" class="black">Lng,Lat 順）
+	      	</td>
+	      </tr>
+	      <tr>
+	      	<td>　　
+	      		<textarea cols="40" rows="8" id="info_window" style="font-size:x-small;"></textarea>
+      		</td>
+  		  </tr>
+	    </table>
+		<hr size="1" class="lightgray">
+    	<div id="directionsPanel" style="margin:3px; font-size:small; width:340px; height:320px; overflow:scroll;">
+      		<div style="font-size:mideum;">Total Distance: <span id="total" style="font-size:small;"></span></div>
+	    </div>
+	  </div>
+	
+	</body>
 </html>
